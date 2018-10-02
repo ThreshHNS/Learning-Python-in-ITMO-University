cb942d39265dd620c27b99380ab87ef19ae3bab8
ThreshHNS <herovelts1@gmail.com>
2018-10-02 20:44:21 +0300

    Removed unnecessary variable @prime in rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,13 +12,10 @@ def is_prime(n):
     >>> is_prime(8)
     False
     """
-    prime = True
     for i in range(2, n):
         if n % i == 0:
-            prime = False
-            break
-    
-    return prime
+        	return False
+    return True
 
 def gcd(a, b):
     """

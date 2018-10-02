75d429724a32f12fc4a38818778cd3b5de6cb5cb
ThreshHNS <herovelts1@gmail.com>
2018-10-02 20:53:11 +0300

    Optimized is_prime() function.

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,7 +12,7 @@ def is_prime(n):
     >>> is_prime(8)
     False
     """
-    for i in range(2, n):
+    for i in range(2, round(n ** .5)):
         if n % i == 0:
         	return False
     return True

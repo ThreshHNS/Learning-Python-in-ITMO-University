13947b18fb1083e3e305eec2e0ca4247e4b41b9c
Andrey Velts <herovelts1@gmail.com>
2019-02-12 01:12:40 +0300

    Завершен функционал по веб-скраппингу

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,27 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
-
-    # PUT YOUR CODE HERE
-
+    news_table = parser.find_all("tr", {"class": "athing"})
+    table_info = parser.find_all("td", {"class": "subtext"})
+    for i in range(len(news_table)):
+        links = table_info[i].find_all("a")
+        title = news_table[i].find(class_="storylink")
+        points = table_info[i].span.text.split()[0]
+        try:
+            if links[5].text == "discuss":
+                comments = "0"
+            else:
+                comments = links[5].text.split()[0]
+        except:
+            comments = "0"
+        news = {
+            'author': links[0].text,
+            'comments': comments,
+            'points': points,
+            'title': title.text,
+            'url': title['href']
+        }
+        news_list.append(news)
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    return parser.table.find_all('table')[1].find_all('a')[-1]['href']
 
 
-def get_news(url, n_pages=1):
+def get_news(url, n_pages=30):
     """ Collect news from a given web page """
     news = []
     while n_pages:
         print("Collecting data from page: {}".format(url))
         response = requests.get(url)
+        print(response)
         soup = BeautifulSoup(response.text, "html.parser")
         news_list = extract_news(soup)
         next_page = extract_next_page(soup)
@@ -29,4 +49,3 @@ def get_news(url, n_pages=1):
         news.extend(news_list)
         n_pages -= 1
     return news
-

08308823bbc00380f4c0f4e8c6742ba6a33fb2f0
Andrey Velts <herovelts1@gmail.com>
2019-02-12 01:13:09 +0300

    Заверешена работа по роутингу и функционалу сервера.

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,10 +1,22 @@
 from bottle import (
-    route, run, template, request, redirect
+    route, run, template, request, redirect, static_file
 )
 
-from scrapper import get_news
+from scraputils import get_news
 from db import News, session
 from bayes import NaiveBayesClassifier
+import string
+
+
+@route('/static/<filename>')
+def server_static(filename):
+    print(filename)
+    return static_file(filename, root='static')
+
+
+@route("/")
+def home_page():
+    return template('index')
 
 
 @route("/news")
@@ -16,21 +28,59 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    s = session()
+    rnews = s.query(News).filter(News.id == request.query.id).first()
+    rnews.label = request.query.label
+    s.commit()
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    news = get_news("https://news.ycombinator.com/newest", n_pages=30)
+    s = session()
+    for item in news:
+        if s.query(News).filter(News.title == item['title'], News.author == item['author']).first():
+            break
+        else:
+            s.add(News(**item))
+    s.commit()
     redirect("/news")
 
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    s = session()
+    labeled_news = s.query(News).filter(News.label != None).all()
+    x_train = [clean(news.title) for news in labeled_news]
+    y_train = [news.label for news in labeled_news]
+    classifier.fit(x_train, y_train)
+
+    rows = s.query(News).filter(News.label == None).all()
+    good, maybe, never = [], [], []
+    for row in rows:
+        [prediction] = classifier.predict([clean(row.title)])
+        if prediction == 'good':
+            good.append(row)
+        elif prediction == "maybe":
+            maybe.append(row)
+        else:
+            never.append(row)
+
+    return template('news_recommendations', good=good, maybe=maybe, never=never)
 
 
-if __name__ == "__main__":
+def clean(s):
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator).lower()
+
+
+if __name__ == '__main__':
+    s = session()
+    labeled_news = s.query(News).filter(News.label != None).all()
+    x_train = [clean(news.title) for news in labeled_news]
+    y_train = [news.label for news in labeled_news]
+    classifier = NaiveBayesClassifier()
+    print(classifier.fit(x_train, y_train))
+    classifier.fit(x_train, y_train)
     run(host="localhost", port=8080)
-
